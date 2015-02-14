78fcd019047354af6865c2f6fe18dc65b4e4886c
Armyornavy <armybunny101@gmail.com>
2015-02-14 11:34:53 -0500

    Create Meganscoutingfile.py

---
+++ Meganscoutingfile.py
@@ -0,0 +1,28 @@
+import os
+import sys
+q1=raw_input ("What is your favorite part of being on Robotics?")
+if q1=="building" or "Building":
+  q1b=raw_input ("What do you like about building?")
+  print q1b
+  if q1b=="working on the robot" or "Working on the robot":
+    q1r=raw_input ("What days would you perfer to come in?")
+    print q1r
+      if q1r=="tuesday, thursday" or "Tuesday, Thursday":
+        q1t= raw_input ("What color would you like the shirts?")
+        print q1t
+        if q1t=="red" or "Red" 
+          q1d=input ("What time would you like to start?")
+          print q1d
+          if q1d=="8"
+print "Thank You!!!"
+      
+    
+    
+    
+    
+   
+    
+  
+
+
+  
